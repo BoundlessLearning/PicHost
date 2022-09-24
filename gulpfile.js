89dcb9ffdc777ee6facbcd26c2f9e8645cb35d57
BoundlessLearning <113996191+BoundlessLearning@users.noreply.github.com>
2022-09-24 23:41:38 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const webp = require('gulp-webp');
 
 gulp.task('default', () => (
-gulp.src('./*.{png,jpg}')
+gulp.src('./Img/*.{png,jpg}')
   .pipe(webp({
             quality: 75,
             preset: 'photo',
             method: 6
         }))
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./Img/'))
 ));
