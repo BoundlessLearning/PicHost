ba6ea3ccb3ea2393cf3478c259249d03ac86b70d
BoundlessLearning <superzombies09@gmail.com>
2022-09-24 16:35:34 +0000

    update

---
+++ gulpfile.js
@@ -0,0 +1,1 @@
+const gulp = require('gulp');const webp = require('gulp-webp');gulp.task('default', () => (gulp.src('./Img/').pipe(webp({quality: 75,preset: 'photo',method: 6})).pipe(gulp.dest('./Img/'))));
