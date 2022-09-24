c6142cd6d64b4402ca4103a1a23711eb8758c4ba
BoundlessLearning <superzombies09@gmail.com>
2022-09-24 17:10:52 +0000

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,1 @@
-const gulp = require('gulp');const webp = require('gulp-webp');gulp.task('default', () => (gulp.src('./Img/明细.png').pipe(webp({quality: 75,preset: 'photo',method: 6})).pipe(gulp.dest('./Img/'))));
+const gulp = require('gulp');const webp = require('gulp-webp');gulp.task('default', () => (gulp.src('./Img/创建账户.png').pipe(webp({quality: 75,preset: 'photo',method: 6})).pipe(gulp.dest('./Img/'))));
