1bb5f40db9438c77a737e2997aa2ac3321751c51
BoundlessLearning <113996191+BoundlessLearning@users.noreply.github.com>
2022-09-25 15:01:08 +0800

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,1 +0,0 @@
-const gulp = require('gulp');const webp = require('gulp-webp');gulp.task('default', () => (gulp.src('./Img/IMGSY1.jpg').pipe(webp({quality: 75,preset: 'photo',method: 6})).pipe(gulp.dest('./Img/'))));
